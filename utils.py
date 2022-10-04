4fc6c7794114ea3169e167cd97358e29ad4ca568
Cesar E Garza <cesar.garza.826@gmail.com>
2022-10-04 03:25:02 -0500

    Started adding typing, refactored some functions, made docstrings more descriptive

--- utils.py
+++ utils.py
@@ -2,12 +2,19 @@
 # https://github.com/frozenpandaman/s3s
 # License: GPLv3
 import base64, datetime, json, re, requests, uuid
+from typing import Optional, Union
 from bs4 import BeautifulSoup
 
 SPLATNET3_URL = "https://api.lp1.av5ja.srv.nintendo.net"
 GRAPHQL_URL  = "https://api.lp1.av5ja.srv.nintendo.net/api/graphql"
 WEB_VIEW_VERSION = "1.0.0-216d0219" # fallback
 S3S_NAMESPACE = uuid.UUID('b3a2dbf5-2c09-4792-b78c-00b548b70aeb')
+
+SUPPORTED_KEYS = [
+    "ignore_private",
+    "app_user_agent",
+    "force_uploads"
+]
 
 # SHA256 hash database for SplatNet 3 GraphQL queries
 # full list: https://github.com/samuelthomas2774/nxapi/discussions/11#discussioncomment-3737698
@@ -22,8 +29,12 @@ translate_rid = {
 	'CoopHistoryDetailQuery':          'f3799a033f0a7ad4b1b396f9a3bafb1e', # SR  / req "coopHistoryDetailId" - query2
 }
 
-def get_web_view_ver():
-	'''Find & parse the SplatNet 3 main.js file for the current site version.'''
+def get_web_view_ver() -> str:
+	'''Find & parse the SplatNet 3 main.js file for the current site version.
+
+	Returns:
+		str: The current site version.
+	'''
 
 	splatnet3_home = requests.get(SPLATNET3_URL)
 	soup = BeautifulSoup(splatnet3_home.text, "html.parser")
@@ -35,16 +46,24 @@ def get_web_view_ver():
 	main_js_url = SPLATNET3_URL + main_js.attrs["src"]
 	main_js_body = requests.get(main_js_url)
 
-	match = re.search(r"\b(?P<revision>[0-9a-f]{40})\b.*revision_info_not_set\"\),.*?=\"(?P<version>\d+\.\d+\.\d+)", main_js_body.text)
-	if not match:
+	pattern = r"\b(?P<revision>[0-9a-f]{40})\b.*revision_info_not_set\"\),.*?=\"(?P<version>\d+\.\d+\.\d+)"
+	match = re.search(pattern, main_js_body.text)
+	if match is None:
 		return WEB_VIEW_VERSION
 
 	version, revision = match.group("version"), match.group("revision")
 	return f"{version}-{revision[:8]}"
 
 
-def set_noun(which):
-	'''Returns the term to be used when referring to the type of results in question.'''
+def set_noun(which: str) -> str:
+	'''Returns the term to be used when referring to the type of results in question.
+
+	Args:
+		which (str): The type of results in question. Can be "ink", "salmon", or "both".
+	
+	Returns:
+		str: The corresponding term to print.
+	'''
 
 	if which == "both":
 		return "battles/jobs"
@@ -54,7 +73,7 @@ def set_noun(which):
 		return "battles"
 
 
-def b64d(string):
+def b64d(string: str) -> Union[str, int]:
 	'''Base64 decode a string and cut off the SplatNet prefix.'''
 
 	thing_id = base64.b64decode(string).decode('utf-8')
@@ -69,16 +88,32 @@ def b64d(string):
 		return int(thing_id) # integer
 
 
-def epoch_time(time_string):
-	'''Converts a playedTime string into an int representing the epoch time.'''
+def epoch_time(time_string: str) -> int:
+	'''Converts a playedTime string into an int representing the epoch time.
+
+	Args:
+		time_string (str): The string to convert. Should be in the format `%Y-%m-%dT%H:%M:%SZ`.
+	
+	Returns:
+		int: The corresponding epoch time.
+	'''
 
 	utc_time = datetime.datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%SZ")
 	epoch_time = int((utc_time - datetime.datetime(1970, 1, 1)).total_seconds())
 	return epoch_time
 
 
-def gen_graphql_body(sha256hash, varname=None, varvalue=None):
-	'''Generates a JSON dictionary, specifying information to retrieve, to send with GraphQL requests.'''
+def gen_graphql_body(sha256hash: str, varname: Optional[str] = None, varvalue: Optional[str] = None) -> str:
+	'''Generates and serializes a JSON dictionary, specifying information to retrieve, to send with GraphQL requests.
+	
+	Args:
+		sha256hash (str): The SHA256 hash of the query to send. A list of hashes can be found in `translate_rid`.
+		varname (Optional[str], optional): The name of the variable to send. Defaults to None.
+		varvalue (Optional[str], optional): The value of the variable to send. Defaults to None.
+	
+	Returns:
+		str: The serialized JSON dictionary.
+	'''
 	great_passage = {
 		"extensions": {
 			"persistedQuery": {
@@ -99,6 +134,6 @@ def custom_key_exists(key, config_data, value=True):
 	'''Checks if a given custom key exists in config.txt and is set to the specified value (true by default).'''
 
 	# https://github.com/frozenpandaman/s3s/wiki/config-keys
-	if key not in ["ignore_private", "app_user_agent", "force_uploads"]:
+	if key not in SUPPORTED_KEYS:
 		print("(!) Checking unexpected custom key")
-	return True if key in config_data and config_data[key].lower() == str(value).lower() else False
+	return config_data.get(key, None) == str(value).lower()
