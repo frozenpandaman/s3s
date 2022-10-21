8161f63f132f2ef67555d74b2c0cc03866e2db0f
Austin <37423484+Toufool@users.noreply.github.com>
2022-10-21 12:03:55 -0400

    use LatestBattleHistoriesQuery in prepare_battle_result if monitoring

--- s3s.py
+++ s3s.py
@@ -547,8 +547,13 @@ def prepare_battle_result(battle, ismonitoring, overview_data=None):
 		payload["rank_exp_change"] = battle["bankaraMatch"]["earnedUdemaePoint"]
 
 		if overview_data is None: # no passed in file with -i
+			#monitoring mode uses LatestBattleHistoriesQuery, so we have to match that in order to match IDs
+			if ismonitoring:
+				query = "LatestBattleHistoriesQuery"
+			else:
+				query = "BankaraBattleHistoriesQuery"
 			overview_post = requests.post(utils.GRAPHQL_URL,
-				data=utils.gen_graphql_body(utils.translate_rid["BankaraBattleHistoriesQuery"]),
+				data=utils.gen_graphql_body(utils.translate_rid[query]),
 				headers=headbutt(),
 				cookies=dict(_gtoken=GTOKEN))
 			try:
@@ -894,7 +899,7 @@ def monitor_battles(which, secs, isblackout, istestrun):
 	if DEBUG:
 		print(f"* monitoring mode start - calling fetch_json() w/ which={which}")
 	# ! fetch from online - no 'specific' = should all be within 'latest'
-	cached_battles, cached_jobs = fetch_json(which, separate=True, specific=True, numbers_only=True, printout=True)
+	cached_battles, cached_jobs = fetch_json(which, separate=True, numbers_only=True, printout=True)
 	if DEBUG:
 		print("* got battle numbers")
 
@@ -920,7 +925,7 @@ def monitor_battles(which, secs, isblackout, istestrun):
 
 			print("Checking for new results...", end='\r')
 			# ! fetch from online
-			ink_results, salmon_results = fetch_json(which, separate=True, specific=True, numbers_only=True) # only numbers or it'd take a long time
+			ink_results, salmon_results = fetch_json(which, separate=True, numbers_only=True) # only numbers or it'd take a long time
 
 			if which in ("both", "ink"):
 				for num in reversed(ink_results):
