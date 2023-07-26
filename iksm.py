819b02db9d9e025c974142cebff1b032bad64c0c
eli fessler <eliwf8@gmail.com>
2023-07-25 16:22:40 -1000

    update splatnet 3 version & query hashes

--- iksm.py
+++ iksm.py
@@ -12,7 +12,7 @@ S3S_VERSION           = "unknown"
 NSOAPP_VERSION        = "unknown"
 NSOAPP_VER_FALLBACK   = "2.6.0"
 WEB_VIEW_VERSION      = "unknown"
-WEB_VIEW_VER_FALLBACK = "4.0.0-d5178440" # fallback for current splatnet 3 ver
+WEB_VIEW_VER_FALLBACK = "4.0.0-22ddb0fd" # fallback for current splatnet 3 ver
 SPLATNET3_URL         = "https://api.lp1.av5ja.srv.nintendo.net"
 
 # functions in this file & call stack:
